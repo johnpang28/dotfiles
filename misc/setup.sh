1412087fbeef0f0a6dfd00b1dff1fe731fb47e41
John Pang <john@pijin.co.uk>
2020-04-08 17:21:15 +0100

    Jetbrains mono font

--- misc/setup.sh
+++ misc/setup.sh
@@ -4,3 +4,7 @@ npm install -g tldr
 
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -P ~/
 mkdir ~/.z
+
+wget https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip -P ~/Downloads -nc
+unzip ~/Downloads/JetBrainsMono-1.0.3.zip -d /tmp
+cp /tmp/JetBrainsMono-1.0.3/ttf/* ~/Library/Fonts/
