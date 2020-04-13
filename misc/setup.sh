26253ae4e6d0c87408ec324dc9d8e7d45e68f7d2
John Pang <john@pijin.co.uk>
2020-04-13 19:33:50 +0100

    jenv config

--- misc/setup.sh
+++ misc/setup.sh
@@ -8,3 +8,6 @@ touch ~/.z
 wget https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip -P ~/Downloads -nc
 unzip ~/Downloads/JetBrainsMono-1.0.3.zip -d /tmp
 cp /tmp/JetBrainsMono-1.0.3/ttf/* ~/Library/Fonts/
+
+jenv enable-plugin export
+
