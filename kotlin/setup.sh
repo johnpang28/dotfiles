84d559f85cdc9c77fb4884d1b241aeda460b9341
John Pang <john@pijin.co.uk>
2021-09-10 01:18:40 +0100

    Kotlin update. Remove jetbrains mono.

--- kotlin/setup.sh
+++ kotlin/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-KOTLIN_VERSION=1.5.10
+KOTLIN_VERSION=1.5.30
 KOTLIN_COMPLIER_ZIP=kotlin-compiler-$KOTLIN_VERSION.zip
 KOTLIN_NATIVE_TAR=kotlin-native-macos-${KOTLIN_VERSION}.tar.gz
 
@@ -17,14 +17,3 @@ sudo ln -s /opt/kotlinc/bin/kotlinc /usr/local/bin/kotlinc
 sudo ln -s /opt/kotlinc/bin/kotlinc-js /usr/local/bin/kotlinc-js
 sudo ln -s /opt/kotlinc/bin/kotlinc-jvm /usr/local/bin/kotlinc-jvm
 
-wget https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VERSION}/${KOTLIN_NATIVE_TAR} -P ~/Downloads -nc
-sudo tar -xvzf ~/Downloads/$KOTLIN_NATIVE_TAR -C /tmp
-sudo mv /tmp/kotlin-native-macos-${KOTLIN_VERSION} /opt
-sudo ln -s /opt/kotlin-native-macos-${KOTLIN_VERSION} /opt/kotlin-native-macos
-sudo ln -s /opt/kotlin-native-macos/bin/cinterop /usr/local/bin/cinterop
-sudo ln -s /opt/kotlin-native-macos/bin/jsinterop /usr/local/bin/jsinterop
-sudo ln -s /opt/kotlin-native-macos/bin/klib /usr/local/bin/klib
-sudo ln -s /opt/kotlin-native-macos/bin/konan-lldb /usr/local/bin/konan-lldb
-sudo ln -s /opt/kotlin-native-macos/bin/konanc /usr/local/bin/konanc
-sudo ln -s /opt/kotlin-native-macos/bin/kotlinc-native /usr/local/bin/kotlinc-native
-sudo ln -s /opt/kotlin-native-macos/bin/run_konan /usr/local/bin/run_konan
--- misc/setup.sh
+++ misc/setup.sh
@@ -5,10 +5,6 @@ npm install -g tldr
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -P ~/
 touch ~/.z
 
-wget https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip -P ~/Downloads -nc
-unzip ~/Downloads/JetBrainsMono-1.0.3.zip -d /tmp
-cp /tmp/JetBrainsMono-1.0.3/ttf/* ~/Library/Fonts/
-
 mkdir -p ~/.jenv/versions
 jenv enable-plugin export
 
