028899f471daba4e020edf3864ab86762aa0c180
John Pang <john@peathree.com>
2023-12-18 18:26:08 +0000

    Use antigen for zsh plugins

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+git clone https://github.com/zsh-users/antigen.git ~/antigen
+
 if [ -f ~/.zshrc ]; then
   cp ~/.zshrc ~/.zshrc.`date -u +"%Y%m%d%H%M%S"`
 fi
