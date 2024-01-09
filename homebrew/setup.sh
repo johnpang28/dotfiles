6f9dadb1116d0c7a860e8eb96eb0efdecbc80500
John Pang <john@pijin.co.uk>
2024-01-09 21:20:10 +0000

    Add homebrew cask

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -12,6 +12,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew update
 brew tap homebrew/bundle
+brew tap homebrew/cask
 brew bundle --file $DIR/Brewfile
 
 sudo ln -s /opt/homebrew/opt/libpq/bin/psql /opt/homebrew/bin/psql
