2d43906da08685609a9b1f699cfbd7e3f824dd9b
John Pang <john@peathree.com>
2024-09-10 21:41:28 +0100

    fzf git integration

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 git clone https://github.com/zsh-users/antigen.git ~/antigen
 
+wget https://raw.githubusercontent.com/junegunn/fzf-git.sh/main/fzf-git.sh -O ~/.fzf-git.sh
+
 if [ -f ~/.zshrc ]; then
   cp ~/.zshrc ~/.zshrc.`date -u +"%Y%m%d%H%M%S"`
 fi
