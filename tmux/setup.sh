873b35af287e4b64ff7de9ee9fb8066aa908fc72
John Pang <john@peathree.com>
2024-09-17 23:54:55 +0100

    addtional tmux config

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -2,5 +2,7 @@
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/catppuccin/tmux.git ~/.tmux/plugins/tmux
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
 
 cp $DIR/.tmux.conf ~/.tmux.conf
