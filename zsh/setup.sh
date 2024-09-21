78e6e2627b8a66968592d4364924d1f8f1dbcfa9
John Pang <john@peathree.com>
2024-09-21 17:16:58 +0100

    Use bat as pager for man

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,4 +4,5 @@ git clone https://github.com/zsh-users/antigen.git ~/antigen
 
 wget https://raw.githubusercontent.com/junegunn/fzf-git.sh/main/fzf-git.sh -O ~/.fzf-git.sh
 
-touch ~/.env
+# use bat as a pager for man
+echo "export MANPAGER=\"sh -c 'col -bx | bat -l man -p'\"" > ~/.env
