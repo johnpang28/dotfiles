79ef989a8de4e3629c5d43418429c65608f72d5a
John Pang <john@peathree.com>
2024-09-25 20:44:49 +0100

    config nvim-tree and telescope

--- stow/home/dot-config/nvim/lua/plugins/nvim-tree.lua
+++ stow/home/dot-config/nvim/lua/plugins/nvim-tree.lua
@@ -1,12 +1,21 @@
 return {
   "nvim-tree/nvim-tree.lua",
   name = "nvim-tree",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("nvim-tree").setup {}
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.opt.termguicolors = true
+
+    require("nvim-tree").setup({
+      view = {
+        width = 50,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+    vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end,
 }
