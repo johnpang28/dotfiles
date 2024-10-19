e9ed68db8c0c033482932b35b4acdcdadbe6e7fa
John Pang <john@peathree.com>
2024-10-19 14:37:03 +0100

    Add lualine

---
+++ stow/home/dot-config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  name = "lualine",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = "catppuccin"
+      }
+    })
+  end,
+}
