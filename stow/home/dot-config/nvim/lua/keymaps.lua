c1b4e413e8d748419033be16df907b0fade52a49
John Pang <john@peathree.com>
2024-11-02 16:29:19 +0000

    keymaps to save and exit vim

---
+++ stow/home/dot-config/nvim/lua/keymaps.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "WQ", ":wq<enter>", {noremap=false})
+
