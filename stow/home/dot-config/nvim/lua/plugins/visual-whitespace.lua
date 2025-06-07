0a1323e51936f3f5723f5a1fd44e4a717acd3e6b
John Pang <john@peathree.com>
2025-06-07 11:45:45 +0100

    Add nvim visual-whitespace plugin

---
+++ stow/home/dot-config/nvim/lua/plugins/visual-whitespace.lua
@@ -0,0 +1,7 @@
+return {
+  "mcauley-penney/visual-whitespace.nvim",
+  name = "visual-whitespace",
+  config = true,
+  event = "ModeChanged *:[vV\22]", -- optionally, lazy load on entering visual mode
+  opts = {},
+}
